658db534e2e3ad699bb74bdbb98ac20e06e2f747
“salmanthecoder” <rahilsalman@gmail.com>
2020-10-16 14:29:27 +0200

    added config for parallel execution

--- src/test/java/com/raptor/ApplicationTest.java
+++ src/test/java/com/raptor/ApplicationTest.java
@@ -3,12 +3,15 @@ package com.raptor;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Execution(ExecutionMode.CONCURRENT)
 public class ApplicationTest extends RaptorBase{
     @BeforeAll
     public void beforeClass()  {
