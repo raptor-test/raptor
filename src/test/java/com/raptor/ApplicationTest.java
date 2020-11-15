f661146e5a3379e31db7bee42cef02dd9fa05914
“salmanthecoder” <rahilsalman@gmail.com>
2020-11-15 02:19:28 +0100

    released 1.0.0 in maven central

--- src/test/java/com/raptor/ApplicationTest.java
+++ src/test/java/com/raptor/ApplicationTest.java
@@ -1,36 +1,36 @@
-package com.raptor;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
-
-@SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ApplicationTest extends RaptorBase{
-    @BeforeAll
-    public void beforeClass()  {
-        initiateReporting("Test-Scans");
-    }
-
-    @AfterAll
-    public void afterClass()  {
-        closeReporting();
-    }
-
-    @Test
-    @DisplayName("testCTScan")
-    public void testCTScan(TestInfo testInfo) throws InterruptedException, IOException {
-        String testFileName = "scans/test-ct-scan.json";
-        runRaptorScript(testFileName,null);
-    }
-
-    @Test
-    @DisplayName("testMriScan")
-    public void testMriScan(TestInfo testInfo) throws InterruptedException, IOException {
-        String testFileName = "scans/test-mri-scan.json";
-        runRaptorScript(testFileName,null);
-    }
-}
+//package com.raptor;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.jupiter.api.*;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.io.IOException;
+//
+//@SpringBootTest
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//public class ApplicationTest extends RaptorBase{
+//    @BeforeAll
+//    public void beforeClass()  {
+//        initiateReporting("Test-Scans");
+//    }
+//
+//    @AfterAll
+//    public void afterClass()  {
+//        closeReporting();
+//    }
+//
+//    @Test
+//    @DisplayName("testCTScan")
+//    public void testCTScan(TestInfo testInfo) throws InterruptedException, IOException {
+//        String testFileName = "scans/test-ct-scan.json";
+//        runRaptorScript(testFileName,null);
+//    }
+//
+//    @Test
+//    @DisplayName("testMriScan")
+//    public void testMriScan(TestInfo testInfo) throws InterruptedException, IOException {
+//        String testFileName = "scans/test-mri-scan.json";
+//        runRaptorScript(testFileName,null);
+//    }
+//}
